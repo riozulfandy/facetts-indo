d53dab882454f32607e8d61bbb38bbebe40fa88c
Muhammad Mariozulfandy <119402060+riozulfandy@users.noreply.github.com>
2024-12-09 14:32:46 +0700

    Update run.py

--- run.py
+++ run.py
@@ -9,11 +9,11 @@ from data import _datamodules
 
 @ex.automain
 def main(_config):
-    config = copy.deepcopy(_config)
+    _config = copy.deepcopy(_config)
     
-    pl.seed_everything(config["seed"])
+    pl.seed_everything(_config["seed"])
     
-    dm = _datamodules["dataset_" + config["dataset"]](config)
+    dm = _datamodules["dataset_" + _config["dataset"]](_config)
     
     
     checkpoint_callback_epoch = pl.callbacks.ModelCheckpoint(
@@ -27,10 +27,10 @@ def main(_config):
     
     lr_callback = pl.callbacks.LearningRateMonitor(logging_interval="step")
     
-    model = FaceTTS(config)
+    model = FaceTTS(_config)
     
     # Manually load state dict if checkpoint only contains state_dict
-    ckpt_path = config.get("resume_from")
+    ckpt_path = _config.get("resume_from")
     if ckpt_path and os.path.exists(ckpt_path):
         checkpoint = torch.load(ckpt_path, map_location='cpu')
         model.load_state_dict(checkpoint['state_dict'])
@@ -40,30 +40,30 @@ def main(_config):
     callbacks = [checkpoint_callback_epoch, lr_callback, model_summary_callback]
     
     num_gpus = (
-        config["num_gpus"] 
-        if isinstance(config["num_gpus"], int) 
-        else len(config["num_gpus"])
+        _config["num_gpus"] 
+        if isinstance(_config["num_gpus"], int) 
+        else len(_config["num_gpus"])
     )
     
-    grad_steps = config["batch_size"] // (
-        config["per_gpu_batchsize"] * num_gpus * config["num_nodes"]
+    grad_steps = _config["batch_size"] // (
+        _config["per_gpu_batchsize"] * num_gpus * _config["num_nodes"]
     )
     
-    max_steps = config["max_steps"] if config["max_steps"] is not None else None
+    max_steps = _config["max_steps"] if _config["max_steps"] is not None else None
     
     trainer = pl.Trainer(
-        gpus=config["num_gpus"],
-        num_nodes=config["num_nodes"],
+        gpus=_config["num_gpus"],
+        num_nodes=_config["num_nodes"],
         strategy=DDPPlugin(gradient_as_bucket_view=True, find_unused_parameters=True),
         max_steps=max_steps,
         callbacks=callbacks,
         accumulate_grad_batches=grad_steps,
         log_every_n_steps=50,
         weights_summary="top",
-        val_check_interval=config["val_check_interval"],
+        val_check_interval=_config["val_check_interval"],
     )
     
-    if not config.get("test_only", False):
+    if not _config.get("test_only", False):
         trainer.fit(model, datamodule=dm)
     else:
         trainer.test(model, datamodule=dm)
